9fb79b1f4e1d6e08adb527c91bae8aad8ff128f7
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-10-27 15:33:58 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,11 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg','apple-touch-icon.png','masked-icon.svg'],
+      includeAssets: [
+        'favicon.svg',
+        'apple-touch-icon.png',
+        'masked-icon.svg'
+      ],
       manifest: {
         name: 'Boundless Grimoire',
         short_name: 'Grimoire',
@@ -24,7 +28,9 @@ export default defineConfig({
           { src: 'maskable-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
         ]
       },
-      workbox: { globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'] }
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json}']
+      }
     })
   ]
 })
