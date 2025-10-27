8da2f57376b4a9efe538f319eedf42e9e1ede280
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-10-27 13:26:39 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
         theme_color: '#0b0b0b',
         background_color: '#0b0b0b',
         display: 'standalone',
-        start_url: '/BoundlessGrimoire/',
+        start_url: '/Boundless-Grimoire/',
         icons: [
           { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
           { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
