5ccba983de63e8370cb991d3af7ada16b3c507a2
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-10-28 14:50:18 +0100

    Update dompurify.d.ts

--- src/dompurify.d.ts
+++ src/dompurify.d.ts
@@ -1,7 +1,3 @@
-// src/types/dompurify.d.ts
-declare module 'dompurify' {
-  export interface DOMPurifyI {
-    sanitize(dirty: string, config?: Record<string, unknown>): string;
-  }
-  export default function createDOMPurify(win?: Window): DOMPurifyI;
-}
+import DOMPurify from 'dompurify'
+// ...
+DOMPurify.sanitize(html, { /* ... */ })
